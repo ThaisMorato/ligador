09e8540314b0903c6e5acb5eb33d88b8d83b21d2
Thais <thaisavilam@gmail.com>
2021-08-10 21:23:54 -0300

    limpando código

--- include/ligador.h
+++ include/ligador.h
@@ -7,7 +7,7 @@
 //linker functions
 int link(char *fileNames[], int fileAmount);
 int firstStep(ifstream &programFile, vector<string> &program, int programStart, StringIntHash &symbolHash);
-void secondStep(vector<string> &program, StringIntHash &symbolHash, string &machineCommands);
+string secondStep(vector<string> &program, StringIntHash &symbolHash);
 void genLinkerOutput(string machineCommands, int machineCommandAmount, StringIntHash &symbolHash);
 void tokenize(string const &str, const char delim, vector<string> &out);
 
--- src/ligador/ligador.cpp
+++ src/ligador/ligador.cpp
@@ -4,7 +4,6 @@
 int link(char *fileNames[], int fileAmount) {
     StringIntHash symbolHash;
     vector<string> program;
-    string machineCommands;
     int machineCommandAmount = 0;
 
     for(int i = 0; i < fileAmount; i++) {
@@ -13,71 +12,38 @@ int link(char *fileNames[], int fileAmount) {
         machineCommandAmount += firstStep(programFile, program, machineCommandAmount, symbolHash);
         programFile.close();
     }
-    secondStep(program, symbolHash, machineCommands);
-    //genLinkerOutput(machineCommands, machineCommandAmount, symbolHash);
+    string machineCommands = secondStep(program, symbolHash);
+    genLinkerOutput(machineCommands, machineCommandAmount, symbolHash);
     return 0;
 }
 
 void genLinkerOutput(string machineCommands, int machineCommandAmount, StringIntHash &symbolHash) {
     int programBegin = symbolHash.getCorrespondent("main");
-    //cout << "MV-EXE\n\n";
+    cout << "MV-EXE\n\n";
     cout << to_string(machineCommandAmount) + " 0 999 " + to_string(programBegin) + "\n\n";
     cout << machineCommands + "\n";
 }
 
 int firstStep(ifstream &programFile, vector<string> &program, int programStart, StringIntHash &symbolHash) {
     string line;
-    size_t pos;
 
-    // //MV-O line
-    // getline(programFile, line);
-    // line.clear();
-
-    // //command line
-    // string command;
-    //getline(programFile, line);
-    // while((pos = line.find(" ")) != string::npos) {
-    //     command = line.substr(0, pos);
-    //     if(!command.empty()) program.push_back(command);
-    //     line.erase(0, pos + 1);
-    // }
-    
+    //command line
+    vector<string> currentProgram;
     getline(programFile, line);
-    tokenize(line,' ', program );
+    tokenize(line,' ', currentProgram);
+    program.insert(program.end(), currentProgram.begin(), currentProgram.end());
     line.clear();
-
-    //teste
-    for(int i = 0; i < (int)program.size(); i++) {
-        cout << program[i] + "|";
-    }
-    cout << endl;
 
     //amount of commands line
     getline(programFile, line);
     int programSize = stoi(line);
-    cout << programSize<<endl;
     line.clear();
-    
-    // //symbol table lines
-    // string label;
-    // string valueString;
-    // int value;
-    // while(getline(programFile, line)) {
-    //     pos = line.find(" ");
-    //     label = line.substr(0, pos);
-    //     line.erase(0, pos + 1);
-    //     valueString = line.find(";");
-    //     value = stoi(valueString);
-    //     symbolHash.addToHash(label, value + programStart);
-    // }
-
 
     //symbol table lines
     string label;
     string valueString;
     vector<string> labelLine;
     int value;
-    
     while(getline(programFile, line)) {
         tokenize(line,' ', labelLine);
         label = labelLine[0];
@@ -86,12 +52,15 @@ int firstStep(ifstream &programFile, vector<string> &program, int programStart, 
         labelLine.clear();
         line.clear();
     }
+
+    return programSize;
 }
 
-void secondStep(vector<string> &program, StringIntHash &symbolHash, string &machineCommands) {
+string secondStep(vector<string> &program, StringIntHash &symbolHash) {
     string command;
     int correspondent;
     int machineCommand;
+    string machineCommands;
     int currentCommand = 1;
     for(size_t i = 0; i < program.size(); i++) {
         command = program[i];
@@ -105,6 +74,7 @@ void secondStep(vector<string> &program, StringIntHash &symbolHash, string &mach
         }
         currentCommand++;
     }
+    return machineCommands;
 }
 
 void tokenize(string const &str, const char delim, vector<string> &out)
